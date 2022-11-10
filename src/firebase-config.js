014b2ca73d880fbe906f1e95cdff3a7677dd84ae
BENBACHIR-HASSANI <fatimazahra.benbachirhassani@ynov.com>
2022-11-10 20:20:02 +0100

    commit1

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUDLI6wG3O6iWmjVoTWLFfZZhgtLCHZ8g",
+  authDomain: "recipes-6de88.firebaseapp.com",
+  projectId: "recipes-6de88",
+  storageBucket: "recipes-6de88.appspot.com",
+  messagingSenderId: "19945772759",
+  appId: "1:19945772759:web:780421fb75ed77cc4ac292",
+  measurementId: "G-1CDK45RFNY",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
